cdbfe553d0db7e9059560100f0e8a759372a08e9
YingLee29 <lequan13576@gmail.com>
2023-08-05 15:06:42 +0700

    create-db-member-project

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,3 @@
-//= require jquery3
-//= require popper
-//= require bootstrap
-
-import "@hotwired/turbo-rails"
-import "@fortawesome/fontawesome-free"
-import "controllers"
+import '@hotwired/turbo-rails';
+import '@fortawesome/fontawesome-free';
+import 'controllers';
---
+++ app/javascript/controllers/user_project_controller.js
@@ -0,0 +1,8 @@
+import { Controller } from '@hotwired/stimulus';
+import 'select2';
+
+export default class extends Controller {
+  connect() {
+    console.log('123');
+  }
+}
