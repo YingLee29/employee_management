4f178939656362d5471f4346e6180d51f45eee3b
YingLee29 <lequan13576@gmail.com>
2023-08-07 22:29:20 +0700

    add-select2

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,8 @@
 import '@hotwired/turbo-rails';
 import '@fortawesome/fontawesome-free';
 import 'controllers';
+import 'jquery';
+import 'jquery_ujs';
+import 'popper';
+import 'bootstrap';
+import 'select2';
--- app/javascript/controllers/user_project_controller.js
+++ app/javascript/controllers/user_project_controller.js
@@ -4,10 +4,20 @@ import 'select2';
 export default class extends Controller {
   connect() {
     $(function () {
-      console.log(123);
       setTimeout(function () {
         $('.hidden-flash').addClass('d-none');
       }, 3000);
+      //select2
+      $('#multiple-select-field').select2({
+        theme: 'bootstrap-5',
+        width: $(this).data('width')
+          ? $(this).data('width')
+          : $(this).hasClass('w-100')
+          ? '100%'
+          : 'style',
+        placeholder: $(this).data('placeholder'),
+        closeOnSelect: false,
+      });
     });
   }
 }
